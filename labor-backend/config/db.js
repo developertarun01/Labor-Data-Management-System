3ca3992e7fd9add0d6c6ba2a7e0173ab33f0e60a
developertarun01 <tarunbusinessmail@gmail.com>
2024-11-24 12:35:37 +0530

    final

--- labor-backend/config/db.js
+++ labor-backend/config/db.js
@@ -12,7 +12,8 @@ const connectDB = async () => {
         const conn = await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 5000, // Add timeout for serverless environment
+            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
@@ -20,6 +21,5 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-
 
 module.exports = connectDB;
