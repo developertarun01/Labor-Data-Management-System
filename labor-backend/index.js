4d299e947d0b39b8b6b3c5739aafef46d3c70f51
developertarun01 <tarunbusinessmail@gmail.com>
2024-11-24 20:45:26 +0530

    Complete

--- labor-backend/index.js
+++ labor-backend/index.js
@@ -7,6 +7,13 @@ require('dotenv').config();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 // Connect to database
 connectDB();
 
