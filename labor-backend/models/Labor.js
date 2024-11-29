823ffe7d5d77746380f1ab5d4496f84d5271c8fd
developertarun01 <tarunbusinessmail@gmail.com>
2024-11-29 17:36:34 +0530

    Trying

--- labor-backend/models/Labor.js
+++ labor-backend/models/Labor.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const laborSchema = new mongoose.Schema({
     name: { type: String, required: true },
     skills: { type: String, required: true },
-    mobile: { type: String, required: true },
+    mobile: { type: String, required: true, unique: true },
     colony: { type: String, required: true },
     city: { type: String, required: true },
 }, { timestamps: true });
